99d784a0289316ac3a81e90c6b609aa26e60353f
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-09-23 14:57:00 +0100

    initial project setup

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img alt="globe travel journal logo" src="../globe.png" />
+      <h1>my travel journal.</h1>
+    </header>
+  );
+}
