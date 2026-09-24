441864a6aa36935fcaf279a387a3e425ed189ea7
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-09-24 12:18:51 +0100

    feat: add data.js for journal entries and render dynamic Entry components with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import Header from "../components/Header.jsx";
 import Entry from "../components/Entry.jsx";
+import data from "../data.js";
 
 export default function App() {
+  const entry = data.map((place) => {
+    return (
+      <Entry
+        key={place.id}
+        alt={place.img.alt}
+        src={place.img.src}
+        title={place.title}
+        country={place.country}
+        googleMapsLink={place.googleMapsLink}
+        dates={place.dates}
+        text={place.text}
+      />
+    );
+  });
+
   return (
     <>
       <Header />
-      <Entry />
+      {entry}
     </>
   );
 }
